04037635f8597fd06204456c99f8e535a3e6c316
rindrajosia <rrindrajosia@gmail.com>
2022-10-30 14:49:30 +0300

    Remove node module

--- part3/backend-telephone/index.js
+++ part3/backend-telephone/index.js
@@ -1,97 +1,149 @@
-const { isNameNotEmpty, isNameUnique } = require('./utils/modules');
-let { personnes } = require('./db');
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const app = express();
+require('dotenv').config()
+const Phone = require('./models/phone')
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
+app.use(express.json())
 
-morgan.token('data', (req, res) => {
-    return JSON.stringify(req.body);
+morgan.token('data', (req, res) => { // eslint-disable-line no-unused-vars
+  return JSON.stringify(req.body)
 })
 
 
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data',{
-  skip: (req, res) => { return req.method !== "POST" }
-}));
+  skip: (req, res) => { return req.method !== 'POST' } // eslint-disable-line no-unused-vars
+}))
+
+app.use(express.static('build'))
 
 
 
-app.get('/api/persons', (req, res) => {
-    res.json(personnes);
+app.get('/api/persons', (req, res, next) => {
+
+  Phone
+    .find({})
+    .then(personnes => {
+      res.json(personnes)
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
-app.get('/api/info', (req, res) => {
-    const number = personnes.length;
-    res.send(`Phonebook has info for ${number} <br/> ${new Date()}`);
+app.get('/api/info', (req, res, next) => {
+
+  Phone
+    .find({})
+    .then(personnes => {
+      res.send(`Phonebook has info for ${personnes.length} <br/> ${new Date()}`)
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const personne = personnes.find(n => n.id === id);
+app.get('/api/persons/:id', (req, res, next) => {
 
-    if(!personne) {
-      return res.status(404).json({
-        error: 'Personne not found'
-      })
-    }
-
-    res.json(personne);
+  Phone.findById(req.params.id)
+    .then(personne => {
+      if(!personne) {
+        return res.status(404).end()
+      } else {
+        res.json(personne)
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    personnes = personnes.filter(n => n.id !== id);
+app.delete('/api/persons/:id', (req, res, next) => {
 
-    res.status(204).end()
+  Phone.findByIdAndRemove(req.params.id)
+    .then(result => {
+      console.log(result)
+      if(result) {
+        res.status(204).end()
+      } else {
+        return res.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
+
+
 })
 
-app.post('/api/persons', (req, res) => {
-    const body = req.body;
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
 
 
-    if(!body) {
-      return res.status(400).json({
-        error: 'content missing'
-      })
-    }
+  const personne = new Phone({
+    name: body.name,
+    number: body.number || 'not defined',
+  })
 
-    if(!isNameNotEmpty(body.name)) {
-      return res.status(400).json({
-        error: 'Name missing'
-      })
-    }
+  console.log(personne)
 
-    if(!isNameUnique(body.name, personnes)) {
-      return res.status(400).json({
-        error: 'name must be unique'
-      })
-    }
-
-
-    const personne = {
-      id: Math.floor(Math.random() * 1000),
-      name: body.name,
-      number: body.number || "not defined",
-    }
-    personnes = personnes.concat(personne);
-
-    res.json(personne);
+  personne.save()
+    .then(savedPhone => {
+      res.json(savedPhone)
+    })
+    .catch(error => {
+      next(error)
+    })
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number || 'not defined',
+  }
+
+  Phone
+    .findByIdAndUpdate(
+      req.params.id,
+      person,
+      { new: true, runValidators: true, context: 'query' }
+    )
+    .then(result => {
+      res.json(result)
+    })
+    .catch(error => next(error))
+
+})
+
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+
+  if(error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message })
+  }
+
+  return next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-console.log(`Server running on port ${3001}`);
+console.log(`Server running on port ${3001}`)
