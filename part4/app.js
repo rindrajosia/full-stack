9b5fec344ec8c43444d397794d6412c3d9119809
rindrajosia <rrindrajosia@gmail.com>
2022-10-30 22:03:19 +0300

    Refactoring Blog List

---
+++ part4/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const app = express()
+
+
+mongoose
+  .connect(config.mongoUrl)
+  .then(result => {
+    logger.info('Connected to mongodb')
+  })
+  .catch(error => {
+    logger.error(error.message)
+  })
+
+
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+
+
+module.exports = app
---
+++ part4/utils/config.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const mongoUrl = process.env.MONGODB_URI
+const PORT = process.env.PORT || 3003
+
+module.exports = {
+  mongoUrl,
+  PORT
+}
